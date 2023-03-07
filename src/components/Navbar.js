7bec97d2e4dbb24aed22b25db3bccd17fd21d910
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-07 08:46:31 -0500

    Fixed linter errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,11 @@
 import { NavLink } from 'react-router-dom';
 
-const links = [
-  { path: '/', text: 'Rockets' },
-  { path: 'missions', text: 'Missions' },
-  { path: 'profile', text: 'My Profile' },
-];
-
 const Navbar = () => (
   <nav className="navbar">
     <ul>
-      {<li>
-          <NavLink
-          >
-          </NavLink>
-        </li>
-      }
+      <li>
+        <NavLink />
+      </li>
     </ul>
   </nav>
 );
