946d4deb4f7109209afe49383039e2ea6fac1988
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-07 19:36:06 +0300

    - New profile route for profile page

---
+++ src/routes/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Profile = () => (
+  <main className="profile-main">
+    <Navbar />
+    <section className="profile-section">Profile</section>
+  </main>
+);
+
+export default Profile;
