9ee2eb19f3b411bbb2cdea9d19254c9ffa1ed0a2
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-08 12:39:35 -0500

    Don't add data to store on every re-render

--- src/routes/Missions.jsx
+++ src/routes/Missions.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getMissionFromApi } from '../redux/missions/missionsSlice';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  dispatch(getMissionFromApi());
+  const missions = useSelector((state) => state.missions.missions);
+
+  if (missions.length === 0) {
+    setTimeout(() => {
+      dispatch(getMissionFromApi());
+    }, '1000');
+  }
 
   return (
     <main className="mission-main">
