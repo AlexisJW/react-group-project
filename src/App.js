9a249630813812bf6e08e50e9c4992acb55d6e3f
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-08 21:42:03 +0300

    - Import rockets route and route it using router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './sass/index.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter, Navigate, Route, Routes,
+} from 'react-router-dom';
 // import store and provider
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Layout from './components/Layout';
 import Profile from './routes/Profile';
 import Missions from './routes/Missions';
+import Rockets from './routes/Rockets';
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Provider store={store}>
           <Routes>
             <Route path="/" element={<Layout />}>
+              <Route index element={<Navigate to="/rockets" />} />
+              <Route path="/rockets" element={<Rockets />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/missions" element={<Missions />} />
             </Route>
